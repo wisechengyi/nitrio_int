8e3d3f28fff28f0a67d14564285fa268d60b8179
wisechengyi <wisechengyi@gmail.com>
2017-03-14 23:58:07 -0700

    fix layer issue

--- ext/content.js
+++ ext/content.js
@@ -17,9 +17,13 @@ chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
         display = "invalid word: " + msg.word;
     }
     $("#scrable-id").remove();
+
+    var current_index_highest = $.topZIndex("div");
+
+
     $('body').prepend(
-        '<div id="scrable-id" style="position: absolute; right: 0; width: 250px;" class="w3-card-4">' +
+        '<div id="scrable-id" style="position: absolute; right: 0; width: 250px; z-index:' + current_index_highest + 1 + ' " class="w3-card-4">' +
         '<header class="w3-container w3-blue">Scrabble info</header>' +
-        '<div class="w3-container">' + display + '</div>' +
+        '<div class="w3-container w3-blue">' + display + '</div>' +
         '</div>');
 });
