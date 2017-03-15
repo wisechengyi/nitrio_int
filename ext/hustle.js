b4c8fb09293264ec63016c6db6af441e98c2067a
wisechengyi <wisechengyi@gmail.com>
2017-03-14 23:36:15 -0700

    add readme

--- ext/hustle.js
+++ ext/hustle.js
@@ -1,7 +1,7 @@
 function getword(info, tab) {
     console.log("Word " + info.selectionText + " was clicked.");
 
-    $.getJSON("http://grabbieteam.com:5000/score_word/" + info.selectionText).done(function (data) {
+    $.getJSON("http://127.0.0.1:5000/score_word/" + info.selectionText).done(function (data) {
 
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, data, function (response) {
@@ -9,10 +9,6 @@ function getword(info, tab) {
         });
 
     });
-
-    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    //     chrome.tabs.sendMessage(tabs[0].id, {action: "SendIt"}, function(response) {});
-    // });
 
 
 }
